7ee6f6470857d277df921eca0204fd896e1c0183
Dictatura <dictatura@yeah.net>
2021-11-14 20:36:17 +0800

    注释掉网络中的norm层

--- networks/RD_Net_GEO.py
+++ networks/RD_Net_GEO.py
@@ -79,9 +79,9 @@ class ActorNet(nn.Module):
         self.layers_gat = nn.ModuleList(self.layers_gat)
 
         self.fc1 = nn.Linear(self.size_layer_list[-1], self.hide_size_fc, bias=False)
-        self.norm1 = nn.BatchNorm1d(self.hide_size_fc)
+        # self.norm1 = nn.BatchNorm1d(self.hide_size_fc)
         self.fc2 = nn.Linear(self.hide_size_fc, self.out_size, bias=False)
-        self.norm2 = nn.BatchNorm1d(self.out_size)
+        # self.norm2 = nn.BatchNorm1d(self.out_size)
 
     def forward(self, x, edge_index, max_size,edge_weight=None):
         for layer in self.layers_gat:
@@ -127,9 +127,9 @@ class CriticNet(nn.Module):
         self.layers_gat = nn.ModuleList(self.layers_gat)
 
         self.fc1 = nn.Linear(self.size_layer_list[-1], self.hide_size_fc, bias=False)
-        self.norm1 = nn.BatchNorm1d(self.hide_size_fc)
+        # self.norm1 = nn.BatchNorm1d(self.hide_size_fc)
         self.fc2 = nn.Linear(self.hide_size_fc, self.out_size, bias=False)
-        self.norm2 = nn.BatchNorm1d(self.out_size)
+        # self.norm2 = nn.BatchNorm1d(self.out_size)
 
     def forward(self, x, edge_index, max_size,edge_weight=None):
         for layer in self.layers_gat:
